5fcd49962898a543625ba6a9bb59a65b94c5c624
Rodrigo <rcarlachiani@outlook.com>
2022-01-06 18:08:50 -0300

    update items II

--- src/components/products.js
+++ src/components/products.js
@@ -12,7 +12,7 @@ export const productos = [
         nombre: 'Los Cabos 5A (Wood tip)', 
         imagen: 'https://i.ibb.co/fDGSjWT/Los-cabos.jpg', 
         precio: '$1250', 
-        stock: 5
+        stock: 5,
         desc: 'Madera: Hickory | Punta: Madera | Longitud: 40.55cm | Diámetro: 1.42cm'
     },
     {
@@ -20,7 +20,7 @@ export const productos = [
         nombre: 'Vater 5A (Wood tip)', 
         imagen: 'https://i.ibb.co/9VnbPDT/Vater.jpg', 
         precio: '$1150', 
-        stock: 8
+        stock: 8,
         desc: 'Madera: Hickory | Punta: Madera | Longitud: 41.00cm | Diámetro: 1.43cm'
     },
     {
@@ -28,7 +28,7 @@ export const productos = [
         nombre: 'Pro-Mark 5A (Wood tip)', 
         imagen: 'https://i.ibb.co/Lgxm91T/Pro-mark.jpg', 
         precio: '$1300', 
-        stock: 3
+        stock: 3,
         desc: 'Madera: Hickory | Punta: Madera | Longitud: 40.84cm | Diámetro: 1.46cm'
     }
 ]
